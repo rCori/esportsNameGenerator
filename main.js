8990c6e2b6093e5b37e52763f22c64127bde173b
RyanCori <ryancori1@gmail.com>
2017-05-07 00:23:26 -0700

    Changed name generation to work in parts

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ var pug = require('pug');
 
 var nameArray = new Array();
 
+var firstNameArray = new Array();
+var lastNameArray = new Array();
+var connectorArray = new Array();
+var composedNameArrayRead = false;
+
 var myAppServer = express();
 var nameArrayRead = false;
 
@@ -16,6 +21,14 @@ function jsonNameRead(err,data){
   nameArrayRead = true;
 }
 
+function jsonNameComposedRead(err,data){
+  if(err) throw err;
+  var jsonComposedNameArrayObj = JSON.parse(data);
+  firstNameArray = jsonComposedNameArrayObj.firstNames;
+  lastNameArray = jsonComposedNameArrayObj.lastNames;
+  connectorArray = jsonComposedNameArrayObj.connectors;
+  composedNameArrayRead = true;
+}
 
 //Get a random name from the name array
 function getRandomName(){
@@ -23,14 +36,27 @@ function getRandomName(){
   return nameArray[selection];
 }
 
+function getRandomComposedName(){
+  var firstName = firstNameArray[Math.floor(Math.random() *  firstNameArray.length)];
+  var lastName = lastNameArray[Math.floor(Math.random() *  lastNameArray.length)];
+  var connector = connectorArray[Math.floor(Math.random() *  connectorArray.length)];
+  var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var sponser = "";
+  for(var i = 0; i < 3; i++){
+    sponser += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length));
+  }
+  return "["+sponser+"]"+firstName+connector+lastName;
+}
+
 fs.readFile('resources/json/fullNames.json','utf8',jsonNameRead);
+fs.readFile('resources/json/nameParts.json','utf8',jsonNameComposedRead);
 
 var compiledEsportsTemplate = pug.compileFile(__dirname + '/resources/pugfiles/index.pug');
 
 myAppServer.get('/esports',function(req,res){
   //If the name array is array then send one
-  if(nameArrayRead){
-    var returnName = getRandomName();
+  if(composedNameArrayRead){
+    var returnName = getRandomComposedName();
     var esportsTemplateOutput = compiledEsportsTemplate({name:returnName});
     res.send(esportsTemplateOutput);
   }
