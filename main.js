7706872fa2e6976e4e6aa570b710069f1d14ca90
RyanCori <ryancori1@gmail.com>
2017-05-07 17:40:35 -0700

    Added connection to mLab MongoDB

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var fs = require('fs');
 var express = require('express');
 var path = require('path');
 var pug = require('pug');
+var MongoClient = require('mongodb').MongoClient;
+
+var mongoUrl = "mongodb://heroku_85s4rvsf:m0iea2ibvu2o3tdp0p92gndq05@ds133291.mlab.com:33291/heroku_85s4rvsf";
 
 var nameArray = new Array();
 
@@ -15,10 +18,38 @@ var myAppServer = express();
 var nameArrayRead = false;
 
 function jsonNameRead(err,data){
-  if(err) throw err;
+  if(err) throw err;1
   var jsonNameArrayObj = JSON.parse(data);
   nameArray = jsonNameArrayObj.nameArray;
   nameArrayRead = true;
+}
+
+function mongodbNameRead(err,db){
+  var firstNameCollection = db.collection('firstNames');
+  firstNameCollection.find({}).toArray(function(err,docs){
+    console.log("Found these first name records:");
+    console.log(docs);
+    for(var i = 0; i<docs.length;i++){
+      firstNameArray.push(docs[i]['firstName']);
+    }
+  });
+  var lastNameCollection = db.collection('lastNames');
+  lastNameCollection.find({}).toArray(function(err,docs){
+    console.log("Found these last name records:");
+    console.log(docs);
+    for(var i = 0; i<docs.length;i++){
+      lastNameArray.push(docs[i]['lastName']);
+    }
+  });
+  var connectorCollection = db.collection('connectors');
+  connectorCollection.find({}).toArray(function(err,docs){
+    console.log("Found these connector records:");
+    console.log(docs);
+    for(var i = 0; i<docs.length;i++){
+      connectorArray.push(docs[i]['connector']);
+    }
+  });
+  composedNameArrayRead = true;
 }
 
 function jsonNameComposedRead(err,data){
@@ -48,8 +79,14 @@ function getRandomComposedName(){
   return "["+sponser+"]"+firstName+connector+lastName;
 }
 
-fs.readFile('resources/json/fullNames.json','utf8',jsonNameRead);
-fs.readFile('resources/json/nameParts.json','utf8',jsonNameComposedRead);
+//fs.readFile('resources/json/fullNames.json','utf8',jsonNameRead);
+//fs.readFile('resources/json/nameParts.json','utf8',jsonNameComposedRead);
+
+MongoClient.connect(mongoUrl,function(err,db){
+  console.log("connected correctly to server");
+  mongodbNameRead(err,db);
+  db.close();
+});
 
 var compiledEsportsTemplate = pug.compileFile(__dirname + '/resources/pugfiles/index.pug');
 
